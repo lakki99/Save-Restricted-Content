a943904e6d05a6fe7ff12b449156d32d1b068d5f
lakki99 <pulka143341@gmail.com>
2025-06-24 23:29:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 import os
 
 # Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7601596122:AAGcwWXISyl8iO_FuqKshCdCRv-xJN4SORA")
 
 # Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", "22432833"))
+API_ID = int(os.environ.get("API_ID", "22554523"))
 
 # Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "897f1c440892cfc46c7e222dfb37d015")
+API_HASH = os.environ.get("API_HASH", "d3cd72e44e4160ce1ee0b7947e8f2d89")
 
 # Your Owner / Admin Id For Broadcast 
-ADMINS = int(os.environ.get("ADMINS", "7592041488"))
+ADMINS = int(os.environ.get("ADMINS", "7460363208"))
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://lavanya143111443:wJ4EhAF0jCUOm35Z@cluster0.llqwyeo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "lakki")
+DB_NAME = os.environ.get("DB_NAME", "lakki1")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
