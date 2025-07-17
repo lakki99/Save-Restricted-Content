000f1f6859d38a8d37380a3912974939b2f07f74
Lakki reddy <pulka143341@gmail.com>
2025-07-17 12:50:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ ADMINS = int(os.environ.get("ADMINS", "7460363208"))
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_NAME = os.environ.get("DB_NAME", "lakki1")
+DB_NAME = os.environ.get("DB_NAME", "lakki9966")
 
 # If You Want Error Message In Your Personal Message Then Turn It True Else If You Don't Want Then Flase
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
