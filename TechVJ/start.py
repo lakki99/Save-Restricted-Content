a832ba1606052059291f1cf173333858d22f3ca7
Lakki reddy <pulka143341@gmail.com>
2026-03-09 20:05:42 +0530

    Update developer contact link in start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -61,7 +61,7 @@ async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
         await db.add_user(message.from_user.id, message.from_user.first_name)
     buttons = [[
-        InlineKeyboardButton("❣️ Developer", url = "https://t.me/lakki_reddy3")
+        InlineKeyboardButton("❣️ Developer", url = "https://t.me/lakki_reddy4")
     ],[
         InlineKeyboardButton('🔍 sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ', url='https://t.me/lakki_redsy3'),
         InlineKeyboardButton('🤖 ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ', url='https://t.me/lakki_reddy3')
